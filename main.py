627b46be3d7e81c0bba9a5b548430a9594509560
Andrii Chebotarov <b00176013@mytudublin.ie>
2026-04-27 14:02:01 +0100

    added name label and input field

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from tkinter import *
 
 root = Tk()
 root.title("My project")
+root.geometry("400x300")
+
+#label
+label = Label(root, text="Enter name:")
+label.pack()
+
+entry = Entry(root)
+entry.pack()
 
 root.mainloop()
