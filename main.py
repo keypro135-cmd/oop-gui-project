9315f1129bfa9ca030951046a21a0f2bdfc8a209
Andrii Chebotarov <b00176013@mytudublin.ie>
2026-04-27 16:15:28 +0100

    added course dropdown menu

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ root.title("My project")
 
 root.geometry("400x300")
 
-#label
+#label_name
 label_name = Label(root, text="Enter name:")
 label_name.pack()
 entry = Entry(root)
@@ -24,5 +24,16 @@ label_year.pack()
 scale_year = Scale(root, from_=1980, to=2009, orient=HORIZONTAL)
 scale_year.pack()
 
+
+# Course (dropdown)
+label_course = Label(root, text="Course:")
+label_course.pack()
+
+course_var = StringVar()
+
+course_var.set("Year 1")
+
+dropdown = OptionMenu(root, course_var, "Year 1", "Year 2", "Year 3", "Year 4")
+dropdown.pack()
 
 root.mainloop()
